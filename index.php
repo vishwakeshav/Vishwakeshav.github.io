429f24cc8a7b521f6afb60a4388e71181892150f
Vishwa Keshav <42441223+vishwakeshav@users.noreply.github.com>
2023-08-17 00:36:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,241 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+
+
+    <title>Vishwa Keshav</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+
+<link href="vk.png" rel="icon">
+<link rel="icon" type="image/x-icon" href="vk.png">
+  <link href="vk.png" rel="apple-touch-icon">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">  
+
+    <style>
+
+      body {
+        
+
+        background: #232526; /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+      }
+
+        .container {
+          width: 100%;
+          height: 100%;
+          padding-right: 15px;
+          padding-left: 15px;
+          margin-right: auto;
+          margin-left: auto;
+        }
+
+        .image {
+          background-image: url("3.jpg");
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-position: center; 
+          width: 100px;
+          height: 100px;
+          border-radius: 50%;
+          margin: 0 auto;
+          border: 5px solid #fdf5e6;
+        }
+
+        a {
+          font-size: large;
+        }
+
+        .w3-border {
+          border: 3px solid #ccc !important;
+        }
+
+        .links-container {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          gap: 20px;
+        }
+
+        .links-container a {
+          width: 100%;
+          color: #414345 !important;
+          transition: 0.2s;
+           border-radius: 10px;
+           white-space: pre-line;
+
+        }
+
+        .links-container a:hover {
+          opacity: 1;
+          background-color: #ccc !important;
+        }
+
+        .profession {
+          margin-top: 15px !important;
+        }
+
+        .profession span {
+          font-size: 14px;
+        }
+
+        .icons {
+          display: flex;
+          justify-content: center;
+          gap: 10px;
+        }
+
+        .icons a {
+          text-decoration: none;
+          margin-right: 20px; /* Increase or decrease this value as needed */
+       display: inline-block;
+  width: 40px; /* Adjust the width and height to control the size of the circular background */
+  height: 40px;
+  border-radius: 50%; /* Apply border-radius to create a circular background */
+  background-color: #f0f0f0; /* Add a background color for the circular background */
+  margin-right: 20px; /* Increase or decrease this value as needed for spacing */
+  text-align: center;
+  line-height: 40px;
+  transition: background-color 0.3s ease; /* Add transition for smooth hover effect */
+}
+
+/* Add additional CSS styling for the icon elements (Optional) */
+.icons i {
+  color: #333; /* Adjust the icon color */
+  font-size: 24px; /* Adjust the icon size */
+}
+
+/* Hover effect - Change background color on hover */
+.icons a:hover {
+  background-color: #cccc; /* Add the desired background color on hover */
+}
+
+        .icons a svg {
+          fill: #fdf5e6 !important;
+          transform: scale(1);
+        }
+
+     
+
+        .contact {
+          text-align: center;
+          color: #fdf5e6;
+        }
+
+        .contact .info {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          gap: 15px;
+        }
+
+        .contact .info span {
+          display: flex;
+          align-items: center;
+          gap: 15px;
+        }
+
+        .bio {
+          font-size: 11px;
+          text-align: center;
+        }
+
+        @media screen and (min-width: 768px) {
+          .link {
+            width: 100%;
+          }
+
+          a {
+            font-size: x-large;
+          }
+
+        }
+        @media screen and (min-width: 576px) {
+          .container {
+            max-width: 540px;
+          }
+
+          .profession span {
+            font-size: 24px;
+          }
+          
+          .bio {
+            font-size: 15px;
+          }
+
+        }
+
+    </style>
+    <script src="https://unpkg.com/feather-icons@4.28.0/dist/feather.min.js"></script>
+  </head>
+  <body>
+    <!-- Content container -->
+    <div class="container">
+
+      <!-- Image and name container -->
+      <div class="w3-margin-top" style="text-align: center">
+        <div class="image w3-round"></div>
+        <p class="profession"><span class="w3-padding w3-margin" style="font-weight: bolder; color: #fdf5e6; font-size: 25px;">Vishwa <span style="color: #ffcc66;font-size: 25px;">Keshav</span></span></p>
+
+      </div>
+
+      <!-- Links section 1 -->
+      <div class="links-container">
+        <a href="https://inizap.com" class="w3-button w3-sand w3-hover-pale-green link border" target="_blank"><b>Inizap</b> - Your One-Stop Tech Solution</a> 
+        <a href="https://panzap.com" class="w3-button w3-sand w3-hover-pale-green link border" target="_blank"><b>Panzap</b> - 20mins Medicine Delivery</a>
+        <a href="https://api.whatsapp.com/send/?phone=917204383856&text=Hi" class="w3-button w3-sand w3-hover-pale-green link border" target="_blank">Tap here to order medicine on WhatsApp with free delivery</a>
+        <a href="https://dietx.ai" class="w3-button w3-sand w3-hover-pale-green link border" target="_blank"><b>Dietx</b> - Your Personalized AI Skincare, Haircare, and Healthcare Assistant.</a>
+        <a href="https://bigwave.in" class="w3-button w3-sand w3-hover-pale-green link border" target="_blank"><b>Bigwave</b> - KCET College Predictor</a>
+        <a href="thoughts.php" class="w3-button w3-sand w3-hover-pale-green link border"><b>Thoughts</b></a>
+         <a href="https://razorpay.me/@bigwavein" class="w3-button w3-sand w3-hover-pale-green link border" target="_blank"><b>Secure Payment Gateway</b> - Explore our secure payment gateway for smooth transactions.</a>
+        
+        
+      </div>
+      <br>
+      <!-- Links section 2 -->
+      <div class="icons w3-padding">
+        <a href="https://twitter.com/vishwa_keshav" target="_blank">
+          <i data-feather="twitter"></i>
+        </a>
+         <a href="https://www.instagram.com/vishwa_keshav/" target="_blank">
+          <i data-feather="instagram"></i>
+        </a>
+        <a href="https://www.facebook.com/thevishwakeshav/" target="_blank">
+          <i data-feather="facebook"></i>
+        </a>
+        
+        <a href="https://www.linkedin.com/in/vishwakeshav/" target="_blank">
+          <i data-feather="linkedin"></i>
+        </a>
+       
+      </div><br>
+      
+        <div class="contact">
+          <p class=""><span class="w3-padding w3-margin" style="font-weight: bolder; color: #fdf5e6">CONTACT</span></p>
+          <div class="info">
+            <!-- <a href = "tel: +919591129698"><span><i data-feather="phone"></i> +91 9591129698</span></a> -->
+            <a href = "mailto: vkeshav@panzap.com"><span><i data-feather="mail"></i> vkeshav@panzap.com</span></a>
+          </div>
+        </div><br>
+      <!-- Footer. This section contains an ad for W3Schools Spaces. You can leave it to support us. -->
+      <footer class="w3-container w3-center w3-padding-48 w3-margin-top">
+        <a class="w3-margin-bottom" href="">
+          <img style="filter: brightness(0) invert(1);" src="vk_2.png" width="80" height="80">
+        </a> 
+      <!-- End footer -->
+      </footer>
+      </div>
+
+    </div>
+    <script>
+      feather.replace()
+    </script>
+  </body>  
+</html>
